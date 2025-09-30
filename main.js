3497f6afc334d1942ef699960489b0c6dac2e0a2
Islabre <islabre1426@gmail.com>
2025-09-30 21:47:06 +0700

    Update, style, and more
    - New Update section
    - style.css: Adding unstyled option, and adjusting style.
    - main.js:
        - Adding comments
        - Remember to apply user-defined style only

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const themesSelection = document.getElementById('themes-selection');
 const themeList = document.querySelectorAll('#themes-selection input[type="radio"]')
 
 if (themeUser != null) {
+    // Remove dev-prefered theme and apply user-setting one
+    document.body.removeAttribute('class');
     document.body.classList.add(themeUser);
 
     // Appropriate setting in themes selection
@@ -17,8 +19,10 @@ themesSelection.addEventListener('change', (e) => {
     if (e.target.checked) {
         const themeChoosen = e.target.value;
 
+        // Remove previously applied theme and set new one
         document.body.removeAttribute('class');
         document.body.classList.add(themeChoosen);
+
         localStorage.setItem('theme', themeChoosen);
     };
 });
