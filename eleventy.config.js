b62c5f9373bcd68ebdf9a56c151f2e3d3353fb21
Islabre <mizuemxc@anonaddy.me>
2026-07-16 18:04:12 +0700

    RSS support and updating website

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,34 @@
 import markdownIt from 'markdown-it';
+import { feedPlugin } from '@11ty/eleventy-plugin-rss';
+
+const markdownOptions = {
+    html: true,
+    linkify: true,
+};
+
+const rssOptions = {
+    collection: {
+        name: 'posts',
+        limit: 0,
+    },
+    metadata: {
+        language: 'en',
+        title: "Islabre's Personal Website",
+        subtitle: 'The personal website of Islabre, a person who likes tech, cuteness, and thinking',
+        base: 'https://islabre.fyi',
+        author: {
+            name: 'Islabre',
+            email: 'islabre1426@disroot.org',
+        },
+    },
+};
 
 export default async function(eleventyConfig) {
-    const markdownOptions = {
-        html: true,
-        linkify: true,
-    };
-
     eleventyConfig.setLibrary('md', markdownIt(markdownOptions));
+    
     eleventyConfig.addPassthroughCopy('src/assets');
+
+    eleventyConfig.addPlugin(feedPlugin, rssOptions);
 }
 
 export const config = {
--- src/posts/index.md
+++ src/posts/index.md
@@ -5,3 +5,5 @@ layout: posts.njk
 ---
 
 This is where I write about tech and life, in reverse-chronological order.
+
+If you want to stay up-to-date, [subscribe to Atom feed](/feed.xml).
